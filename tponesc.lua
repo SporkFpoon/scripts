d1395a11deafa2212eedbacde19fb1d8b2f37de8
SporkFpoon <storeforiluminateen@gmail.com>
2024-11-16 20:34:32 -0300

    Update tponesc.lua

--- tponesc.lua
+++ tponesc.lua
@@ -1,6 +1,6 @@
 local ts = game:GetService("TeleportService")
 local gs = game:GetService("GuiService")
-local player = Players.LocalPlayer
+local player = game:GetService("Players").LocalPlayer
 
 gs.MenuOpened:Connect(function()
     ts:TeleportToPlaceInstance(game.PlaceId, game.JobId, player)
